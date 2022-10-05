d16289b533ce3e804b5be74d779b548962c7bb3b
Rich Vincent D. Flores <69899683+cruelCase@users.noreply.github.com>
2022-10-05 09:53:04 +0800

    Update Comparison.java

--- Comparison.java
+++ Comparison.java
@@ -14,10 +14,10 @@ public class Comparison {
     int b = dex.nextInt();
     
     if (a > b) {
-     System.out.println(a + " is greater than " + b);
+     System.out.println(a + " is greater);
      
    }else if (a < b){
-     System.out.println(b + " is greater than " + a);
+     System.out.println(b + " is greater);
    } else {
      System.out.println("They are equal");
    }
