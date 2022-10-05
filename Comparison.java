6e0bfef1f5321c10e98247a34d94e052f8a16440
Rich Vincent D. Flores <69899683+cruelCase@users.noreply.github.com>
2022-10-05 09:41:25 +0800

    Add files via upload

---
+++ Comparison.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class Comparison {
+
+  public static void main(String[] args) {
+    Scanner dex = new Scanner(System.in);
+    
+    System.out.print("Enter first Integer: ");
+    int a = dex.nextInt();
+    
+    System.out.print("Enter second Integer: ");
+    int b = dex.nextInt();
+    
+    if (a > b) {
+     System.out.println(a + " is greater than " + b);
+     
+   }else if (a < b){
+     System.out.println(b + " is greater than " + a);
+   } else {
+     System.out.println("They are equal");
+   }
+    
+    
+  }
+
+}
